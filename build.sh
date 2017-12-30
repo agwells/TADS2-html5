0b02d587a2b7476267cae183e81f376c148c5359
Aaron Wells <aaronw@catalyst.net.nz>
2017-12-30 16:23:53 +1300

    Adding code to export "cwrap"
    ... to resolve an error that occurs otherwise.

--- build.sh
+++ build.sh
@@ -21,4 +21,4 @@ emcc ${TADSDIR}bifgdum.c ${TADSDIR}osgen3.c ${TADSDIR}os0.c ${TADSDIR}osnoui.c $
 emcc obj/cmnrun.o obj/run.o obj/char.o -I${TADSUNIXDIR} -I${TADSDIR}  -o obj/tr.o  ${CCOPTS}
 
 
-emcc obj/tr.o -o bin/lib/tr.js -s EXPORTED_FUNCTIONS="['_tads_worker_main']" --js-library ${TADSES6}/tadsthunk.js ${LDOPTS}
+emcc obj/tr.o -o bin/lib/tr.js -s EXPORTED_FUNCTIONS="['_tads_worker_main']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap']" --js-library ${TADSES6}/tadsthunk.js ${LDOPTS}
